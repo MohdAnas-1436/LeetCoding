1904aba40e6b00b4dbc4b154aa0769320fcdcaac
Mohd Anas <mohdanas8726@gmail.com>
2026-03-26 11:50:54 +0530

    Time: 0 ms (100%), Space: 15.4 MB (8.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,30 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+// Compiler directive to speed up I/O operations before main() executes
+static const int __ = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
         std::unordered_map<int, int> numMap;
+        
+        // Prevent expensive rehashing by allocating memory upfront
+        numMap.reserve(nums.size());
         
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
-            if (numMap.find(complement) != numMap.end()) {
+            
+            if (numMap.count(complement)) {
                 return {numMap[complement], i};
             }
+            
             numMap[nums[i]] = i;
         }
         
