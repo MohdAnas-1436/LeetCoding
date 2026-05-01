5030d88ed858cd087a3411898c656b66e02e4529
Mohd Anas <mohdanas8726@gmail.com>
2026-05-01 10:02:53 +0530

    Time: 0 ms (100%), Space: 14.5 MB (74.74%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
@@ -1,3 +1,40 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     struct TreeNode *left;
+//  *     struct TreeNode *right;
+//  * };
+//  */
+
+// int answer;
+
+// int height(struct TreeNode* node) {
+//     // code here
+//     if (node == NULL) return 0;
+    
+//     int lh = height(node->left);
+//     int rh = height(node->right);
+//     int maxHeight = (lh > rh)? lh : rh;
+//     return 1 + maxHeight;
+// }
+
+// void diameter(struct TreeNode *root){
+//     if (root == NULL) return;
+//     int lh = height(root->left);
+//     int rh = height(root->right);
+//     int dia = lh + rh;
+//     answer = (answer < dia)? dia : answer;
+//     diameter(root->left);
+//     diameter(root->right);
+// }
+
+// int diameterOfBinaryTree(struct TreeNode* root) {
+//     answer = 0;
+//     diameter(root);
+//     return answer;
+// }
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,28 +46,27 @@
 
 int answer;
 
+// Optimized to calculate height AND update diameter at the same time
 int height(struct TreeNode* node) {
-    // code here
     if (node == NULL) return 0;
     
     int lh = height(node->left);
     int rh = height(node->right);
-    int maxHeight = (lh > rh)? lh : rh;
-    return 1 + maxHeight;
-}
-
-void diameter(struct TreeNode *root){
-    if (root == NULL) return;
-    int lh = height(root->left);
-    int rh = height(root->right);
-    int dia = lh + rh;
-    answer = (answer < dia)? dia : answer;
-    diameter(root->left);
-    diameter(root->right);
+    
+    // The diameter passing through this current node
+    int current_diameter = lh + rh;
+    
+    // Update the global maximum diameter if the current one is larger
+    if (current_diameter > answer) {
+        answer = current_diameter;
+    }
+    
+    // Return the height of the current node
+    return 1 + (lh > rh ? lh : rh);
 }
 
 int diameterOfBinaryTree(struct TreeNode* root) {
-    answer = 0;
-    diameter(root);
+    answer = 0; // Reset for multiple test cases
+    height(root); 
     return answer;
 }
