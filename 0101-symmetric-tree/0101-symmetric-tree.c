02adfcaecb6e946b5d1b8685cd1b3052a3517be3
Mohd Anas <mohdanas8726@gmail.com>
2026-05-07 14:51:49 +0530

    Time: 0 ms (100%), Space: 10.8 MB (68.18%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.c
+++ 0101-symmetric-tree/0101-symmetric-tree.c
@@ -7,28 +7,23 @@
  * };
  */
 
-// The helper function to compare two nodes
-bool isMirror(struct TreeNode* p1, struct TreeNode* p2) {
-    // If both nodes are NULL, they are symmetric
+bool symmetric(struct TreeNode* p1, struct TreeNode* p2) {
     if (p1 == NULL && p2 == NULL) {
         return true;
     }
-    
-    // If only one is NULL, or if their values don't match, they are not symmetric
-    if (p1 == NULL || p2 == NULL || (*p1).val != (*p2).val) {
+
+    if (p1 == NULL || p2 == NULL || p1->val != p2->val) {
         return false;
     }
     
-    // Recursively check the outer edges and inner edges
-    return isMirror((*p1).left, (*p2).right) && isMirror((*p1).right, (*p2).left);
+    return symmetric(p1->left, p2->right) && symmetric(p1->right, p2->left);
 }
 
-// The main function
 bool isSymmetric(struct TreeNode* root) {
     if (root == NULL) {
         return true;
     }
     
     // Call the helper function with the left and right children of the root
-    return isMirror((*root).left, (*root).right);
+    return symmetric(root->left, root->right);
 }
